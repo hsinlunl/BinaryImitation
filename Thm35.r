bc3a8b1066b3b9594355d5d35ac9c70f5f71f7e1
hsinlunl <57415904+hsinlunl@users.noreply.github.com>
2025-05-17 17:24:39 +0800

    Update Thm35.r

--- Thm35.r
+++ Thm35.r
@@ -5,7 +5,7 @@ q_s <- 0.8
 q_d <- 0
 n <- 1000
 simulations <- 1000
-max_iter <- 30000
+max_iter <- 20000
 
 # Function: check if current state is a global tie
 is_global_tie <- function(g, opinions) {
